2bffce00399a4c2f960838c63c0a85758e4c46dd
Antoine Poussard <antoine.poussard@gmail.com>
2020-04-27 11:07:05 +0200

    [FIX] working backend

--- backend/src/server/views.py
+++ backend/src/server/views.py
@@ -7,14 +7,11 @@ import requests
 
 from config.settings import SETTINGS
 from flask import request, jsonify, abort, Blueprint, send_file
-from server import *
 from server.extensions import rest_api
 
 LOG = logging.getLogger(__name__)
 
 blueprint = Blueprint('views', __name__)
-# rest_api.add_resource(image_view.ImageView, '/cam_image')
-# rest_api.add_resource(image_view.ImageSettings, '/image_settings')
 
 @blueprint.route('/status/is_up', methods=['GET'])
 def is_up():
