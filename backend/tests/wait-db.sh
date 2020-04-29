d55e34199048644e1ba76fca17472bf797850110
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-04-29 16:10:22 +0200

    name database and user changed

--- backend/tests/wait-db.sh
+++ backend/tests/wait-db.sh
@@ -5,7 +5,7 @@ while [[ $res != *"1 row"* ]];do
   echo "Waiting for database connection..."
   # wait for 5 seconds before check again
   sleep 2
-  res=`docker exec covid_postgres_test psql -U aiosuperuserstrike aiostrike -c 'SELECT count(*) FROM account limit 1;'`
+  res=`docker exec covid_postgres_test psql -U aiosuperusercovid aiocovid -c 'SELECT count(*) FROM account limit 1;'`
 done
 echo "DATABASE SUCCESSFULLY CONNECTED"
 sleep 20
