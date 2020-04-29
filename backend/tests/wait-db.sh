067d3834112ba3ae614f6f9ee25443cc847faea9
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-04-29 15:29:22 +0200

    wait-db kaizen

--- backend/tests/wait-db.sh
+++ backend/tests/wait-db.sh
@@ -6,7 +6,7 @@ while [[ $res != *"1 row"* ]];do
   # wait for 5 seconds before check again
   sleep 2
   res=`docker exec covid_postgres_test psql -U aiosuperuserstrike aiostrike -c 'SELECT count(*) FROM account limit 1;'`
-  echo $res
 done
+echo "DATABASE SUCCESSFULLY CONNECTED"
 sleep 20
 ###
