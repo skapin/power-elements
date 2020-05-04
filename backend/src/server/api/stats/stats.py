8a344c027d28f65f7409efa0b6b5c10f5f7c8e0a
Antoine Poussard <antoine.poussard@gmail.com>
2020-05-04 14:36:44 +0200

    [FEATURE] get stats for admin page

--- backend/src/server/api/stats/stats.py
+++ backend/src/server/api/stats/stats.py
@@ -8,6 +8,7 @@ from common.utils.security import generate_user_token, extract_payload
 from db.models import Question, Account, Response
 from common.db.base import Database
 from flask_restful_swagger import swagger
+from sqlalchemy import desc, func
 
 LOG = logging.getLogger(__name__)
 
@@ -25,7 +26,7 @@ class Stats(Resource):
             }
         ]
     )
-    def get(self):
+    def post(self):
         return_list = []
         with Database(auto_commit=True) as db:
 
@@ -38,15 +39,14 @@ class Stats(Resource):
                   .join(Account)\
                   .filter(Response.question_id == question.uniqid)\
                   .all()
-                
+
                 answers = []
                 for row in stats:
-                    answers.append({
-                                        'value': row.value,
-                                        'created_at': row.created_at,
-                                        'username': row.account.name
-                                        })
-                
+                    answers.append({'value': row.value,
+                                    'created_at': row.created_at,
+                                    'username': row.account.name
+                                    })
+
                 return_list.append({
                   'index': num,
                   'question_id': question.uniqid,
@@ -58,3 +58,12 @@ class Stats(Resource):
 
 
         return jsonify(return_list)
+
+    def get(self):
+        with Database(auto_commit=True) as db:
+            all_answers = db.query(func.date(Response.created_at), func.count(Response.value))\
+                            .filter_by(value=100)\
+                            .group_by(func.date(Response.created_at))\
+                            .all()
+            LOG.info(all_answers)
+            return jsonify(all_answers)
